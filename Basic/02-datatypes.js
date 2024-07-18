3351f625fdd05c41d38fe218fdc69fd6508b6a4e
VictorDev <sismo76@hotmail.com>
2024-07-17 20:38:01 -0600

    Tryin to keep it up

---
+++ Basic/02-datatypes.js
@@ -0,0 +1,16 @@
+// Cadenas de texto (string)
+let name = "Brais Moure"
+let alias = 'MoureDev'
+let email = 'braismoure@mouredev.com'
+
+// Numeros (number)
+let age = 37 // Entero
+let height = 1.77 // Decimal
+
+// booleanos (boolean)
+let isTeacher = true
+let isStudent = false
+
+// Undefined
+let undefinedValue
+console.log(undefindedValue)
