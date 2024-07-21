142827334b58217e48cfed1f6325503749be4ec9
VictorDev <sismo76@hotmail.com>
2024-07-20 19:49:48 -0600

    Poniendo al dia el curso

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -5,18 +5,41 @@ https://www.twitch.tv/videos/2200149072?t=00h08m02s
 */
 
 // 1. Escribe un comentario en una línea
-
+'Comentario'
 // 2. Escribe un comentario en varias líneas
-
+/*Comentario
+en varias líneas"*/
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
+let numero = 3
+let cadena = 'String'
+let GranEntero = BigInt(25412345123451254)
+let Booleano = true
 
 // 4. Imprime por consola el valor de todas las variables
+console.log(numero)
+console.log(cadena)
+console.log(GranEntero)
+console.log(Booleano)
 
 // 5. Imprime por consola el tipo de todas las variables
+console.log(typeof(numero))
+console.log(typeof(cadena))
+console.log(typeof(GranEntero))
+console.log(typeof(Booleano))
 
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo
+numero = 5
+cadena = 'Otro String'
+GranEntero = BigInt(513245123451)
+Booleano = false
+console.log(typeof(numero))
 
 // 7. A continuación, modifica los valores de las variables por otros de distinto tipo
+numero = '5'
+cadena = 12
+GranEntero = 'string'
+Booleano = 'no se'
+console.log(typeof(numero))
 
 // 8. Declara constantes con valores asociados a todos los tipos de datos primitivos
 
