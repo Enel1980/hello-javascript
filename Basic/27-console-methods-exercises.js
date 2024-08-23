654a52b104dbf96f0285827f89b2d3b3d7f63234
VictorDev <sismo76@hotmail.com>
2024-08-23 17:16:09 -0600

    Fin del curso

--- Basic/27-console-methods-exercises.js
+++ Basic/27-console-methods-exercises.js
@@ -5,21 +5,70 @@ https://www.youtube.com/live/PAnxhBE5kIE?si=V0F_NsKO9lmhhatu&t=555
 */
 
 // 1. Crea un función que utilice error correctamente
-
+function restaNatural(a, b) {
+    if (b > a) {
+        console.error("Esto es un error")
+    }
+    else console.log(a - b)
+}
+restaNatural(3, 2)
+restaNatural(2, 3)
 // 2. Crea una función que utilice warn correctamente
+function restaEntera(a, b) {
+    if (b > a) {
+        console.warn("Esto no funcionaría en la resta natural")
+    }
+    console.log(a - b)
+}
+restaEntera(3, 2)
+restaEntera(2, 3)
 
 // 3. Crea una función que utilice info correctamente
+function restaGeneral(a, b) {
+    if (b > a) {
+        console.info("Es una resta entera")
+    }
+    else console.info("Es como una resta natural")
+    console.log(a - b)
+}
+restaGeneral(3, 2)
+restaGeneral(2, 3)
 
 // 4. Utiliza table
+let datos = [
+    { nombre: "El dato", cantidad: 3 },
+    { nombre: "El otro dato", cantidad: 4 }
+]
+console.table(datos)
 
 // 5. Utiliza group
+console.group("Esto")
+console.log("Esto, indentado")
+console.log("Esto indentado igual")
+console.groupEnd()
+console.log("Se acabó y volvemos a empezar")
+
 
 // 6. Utiliza time
+console.time("ejecucion")
+
+console.log("Solo esto")
+
+console.timeEnd("ejecucion")
 
 // 7. Valida con assert si un número es positivo
+let numero = 7
+console.assert((numero <= 0), "Es positivo")
 
 // 8. Utiliza count
+console.count("1001")
+console.count("1001")
+console.count("1001")
+console.countReset("1001")
+console.count("1001")
 
 // 9. Utiliza trace
+console.trace("ahora")
 
 // 10. Utiliza clear
+console.clear()
--- Basic/31-modules-exercises.js
+++ Basic/31-modules-exercises.js
@@ -5,16 +5,43 @@ https://www.youtube.com/live/PAnxhBE5kIE?si=V0F_NsKO9lmhhatu&t=555
 */
 
 // 1. Exporta una función
+export function sub(a, b) {
+    return a - b
+}
 
 // 2. Exporta una constante
+export const ElPi = 3.141692
 
 // 3. Exporta una clase
+export class CircularSector {
+
+    constructor(radius, angle) {
+        this.radius = radius
+        this.angle = angle
+    }
+
+    area() {
+        return Math.PI * this.radius * angle / 360
+    }
+
+    longArc() {
+        return 2 * Math.PI * this.radius * angle / 360
+    }
+
+}
 
 // 4. Importa una función
+import { add } from "./28-export-modules.js"
+console.log(add(1, 2))
 
 // 5. Importa una constante
+import { PI } from "./28-export-modules.js"
+console.log(PI)
 
 // 6. Importa una clase
+import { Circle } from "./28-export-modules.js"
+let circle2 = new Circle(1)
+console.log(circle2.area())
 
 // 7. Exporta una función, una constante y una clase por defecto (en caso de que lo permita)
 
